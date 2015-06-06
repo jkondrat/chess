704eec6b636aeebd470ef7636eafec957c31dec2
Jakub Kondrat <kondratjakub@gmail.com>
2015-06-06 22:07:34 +0200

    structure

---
+++ server.js
@@ -0,0 +1,15 @@
+var app = require("express")();
+var httpServer = require("http").Server(app);
+var io = require("socket.io")(httpServer);
+
+var static = require('serve-static');
+var port = process.env.PORT || 8000;
+
+app.use('/img', static(__dirname + '/public/img', { maxAge: 86400000 }));
+app.use('/js/jquery.min.js', static(__dirname + '/bower_components/jquery/dist/jquery.min.js'));
+app.use('/js/jquery.min.map', static(__dirname + '/bower_components/jquery/dist/jquery.min.map'));
+app.use(static(__dirname + '/public'));
+
+httpServer.listen(port, function () {
+	console.log("test");
+});
