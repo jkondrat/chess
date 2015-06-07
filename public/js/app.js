56de6b699a512b1532104eec6d2cff2fb028f113
Jakub Kondrat <kondratjakub@gmail.com>
2015-06-07 19:15:39 +0200

    draggable pieces, simple game logic for client

--- public/js/app.js
+++ public/js/app.js
@@ -1,14 +1,73 @@
 var app = angular.module('chess', []);
 
 app.controller('ChessController', ['$scope', function($scope) {
-	$scope.chars = ['H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'];
-	$scope.ranks = [1, 2, 3, 4, 5, 6, 7, 8];
+	$scope.rows = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h'];
+	$scope.ranks = [8, 7, 6, 5, 4, 3, 2, 1];
 
+	var chess = new Chess();
 	var socket;
+	var validMoves = [];
+
+	function validateMove(fromTile, toTile) {
+		if (fromTile.get(0) !== toTile.get(0)) {
+			var sqTo = toTile.data("sq");
+			return ($.inArray(sqTo, validMoves) != -1);
+		}
+		return false;
+	}
+
+	function initBoard() {
+		$.each($(".tile"), function(i, tile) {
+			var tileEl = $(tile);
+			var sq = tileEl.data("sq");
+			var piece = chess.get(sq);
+			if (piece != null) {
+				var pieceEl = $('<span />');
+				pieceEl.addClass('piece');
+				if (piece.color == 'b') {
+					pieceEl.addClass('black');
+				} else {
+					pieceEl.addClass('white');
+				}
+				pieceEl.addClass(piece.type);
+				tileEl.empty();
+				tileEl.append(pieceEl);
+			}
+		});
+
+		$( ".piece" ).draggable({
+			revert: 'invalid',
+			start: function( event, ui ) {
+				validMoves = [];
+				var sq = $(this).parent().data("sq");
+				var moves = chess.moves({square: sq, verbose: true});
+				$.each(moves, function(i, m) {
+					validMoves.push(m.to);
+				});
+			}
+		});
+		$( ".tile" ).droppable({
+			accept: function (el) {
+				return validateMove(el.parent(), $(this));
+			},
+			drop: function(event, ui) {
+				var draggable = ui.draggable;
+				var droppable = $(this);
+				var sqFrom = draggable.parent().data("sq");
+				var sqTo = droppable.data("sq");
+				chess.move({ from: sqFrom, to: sqTo });
+				
+				droppable.empty();
+				$(draggable).detach().css({top:0, left:0}).appendTo(droppable);
+  			}
+		});
+	}
 
 	window.addEventListener("load", function (event) {
-	    if (!socket || !socket.connected) {
-	        socket = io({forceNew: true});
-	    }
+		if (!socket || !socket.connected) {
+			socket = io({forceNew: true});
+		}
+
+		initBoard();
 	});
 }]);
--- server.js
+++ server.js
@@ -11,8 +11,10 @@ var port = process.env.PORT || 8000;
 app.use('/img', static(__dirname + '/public/img', { maxAge: 86400000 }));
 app.use('/js/jquery.min.js', static(__dirname + '/bower_components/jquery/dist/jquery.min.js'));
 app.use('/js/jquery.min.map', static(__dirname + '/bower_components/jquery/dist/jquery.min.map'));
+app.use('/js/jquery-ui.min.js', static(__dirname + '/bower_components/jquery-ui/jquery-ui.min.js'));
 app.use('/js/angular.min.js', static(__dirname + '/bower_components/angularjs/angular.min.js'));
 app.use('/js/angular.min.map', static(__dirname + '/bower_components/angularjs/angular.min.js.map'));
+app.use('/js/chess.min.js', static(__dirname + '/bower_components/chess.js/chess.min.js'));
 app.use(lessMiddleware(path.join(__dirname, 'src'), {
 	dest: path.join(__dirname, 'public')
 }));
